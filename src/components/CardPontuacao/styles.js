ca09e43a45490d7947a50b7bcfaf03473c2a9ecf
Nery <felipe.nery@facamara.com.br>
2020-09-03 18:48:01 -0300

    melhoria layout card pontuacao
    falta mobile

--- src/components/CardPontuacao/styles.js
+++ src/components/CardPontuacao/styles.js
@@ -6,25 +6,35 @@ export default makeStyles((theme) => ({
         flexDirection: 'column',
         textAlign: 'left',
         padding: '20px',
+        backgroundColor: '#fff'
     },
     cardPontuacaoTitulo: {
         fontStyle: 'italic',
-        color: '#fff',
-        fontWeight: 'bold',
-        textTransform: 'uppercase'
+        fontSize: '14px',
+        color: '#999',
+        textTransform: 'uppercase',
+        textAlign: 'right'
     },
     cardPontuacaoValor: {
+        textAlign: 'right',
         '& p': {
             fontSize: '30px'
         }
        
     },
     cardPontuacaoIcone: {
+        boxShadow: '0 4px 20px 0 rgba(0, 0, 0,.14), 0 7px 10px -5px rgba(156, 39, 176,.4)',
+        color: '#fff',
+        display: 'flex',
+        alignItems: 'center',
+        padding: '15px',
+        marginTop: '-100px',
+        borderRadius: '3px',
+        marginRight: '15px',
+        width: '70px',
+        float: 'left',
         '& svg':{
-            float: 'right',
-            marginTop: '-45px',
             fontSize: '35px',
-            color: '#fff'
         }
     }
 }));
